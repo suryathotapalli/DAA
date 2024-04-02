c84a84be6f71ff21392b1d6a2870ba3ee368ed3c
suryathotapalli <158138287+suryathotapalli@users.noreply.github.com>
2024-04-02 17:49:43 +0530

    Delete PART 1 directory

--- PART 1/P1_Q4.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(s):
-    if len(s) == 0:
-        return s
-    else:
-        return reverse_string(s[1:]) + s[0]
-
-input_string = "pots&pans"
-reversed_string = reverse_string(input_string)
-print(reversed_string)
